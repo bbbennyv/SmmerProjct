9ea7f0536e4ffad3c119b5c8d98cf8895b53b925
Lschaaf24 <Liam2.Schaaf@live.uwe.ac.uk>
2026-04-28 16:48:31 +0100

    Movement and Double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
+    [SerializeField]
+    private float movementSpeed = 500.0f;
+    [SerializeField]
+    private float jumpHeight = 10.0f;
+
+    private float horizontalMovement; 
+    private float verticalMovement;
+
+    private bool isGrounded = true;
+
+    private int jumpsToUse = 2;
+
+    private Rigidbody2D rb;
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+       rb = GetComponent<Rigidbody2D>();
     }
 
     public void Move(InputAction.CallbackContext action)
@@ -14,6 +27,11 @@ public class PlayerController : MonoBehaviour
         //^read inputs
         //code in what to do with the input
         //its in vector2 so it should be easier for you
+
+        horizontalMovement = action.ReadValue<Vector2>().x;
+
+        //rb.linearVelocity = new Vector2(horizontalMovement * movementSpeed, rb.linearVelocity.y);
+
         Debug.Log("MOVEMENT");
     }
 
@@ -22,13 +40,33 @@ public class PlayerController : MonoBehaviour
         //^read jump input
         //code in what to do with the input
         //i assume its on pressed so..
+
+        
+        if (action.started) { 
+        
+            if(jumpsToUse > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+                jumpsToUse --;
+            }
+        }
+
         Debug.Log("Jump");
 
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpsToUse = 2;
+        }
+    }
 
     void Update()
     {
-        
+
+        rb.linearVelocity = new Vector2(horizontalMovement * movementSpeed, rb.linearVelocity.y);
+        Debug.Log(isGrounded);
     }
 }
